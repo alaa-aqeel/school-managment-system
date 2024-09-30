e34be28a399a10de6cdfef9de0c605ddf173b73b
Alaa Aqeel <alaa.21.iraq@gmail.com>
2024-10-01 01:21:06 +0300

    refactory (sh): create one sh script to handler cli app

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo docker compose -f docker-compose.dev.yml up --build
+case "$1" in
+    "up")
+        sudo docker compose -f docker-compose.dev.yml up --build
+        ;;
+    "test")
+        # Run migrate down to undo the last migration
+        sudo docker compose -f docker-compose.test.yml up --build
+        ;;
+    *)
+        echo "Usage: $0 {start|test}"
+        exit 1
+        ;;
+esac
--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker compose -f docker-compose.test.yml up --build
